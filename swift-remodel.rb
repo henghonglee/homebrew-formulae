24df7b2164eafbfc7fd7cff5310f23da199f2748
henghonglee <henghong.lee@gmail.com>
2019-01-02 17:03:10 +0800

    update to 1.1

--- swift-remodel.rb
+++ swift-remodel.rb
@@ -1,7 +1,7 @@
 class SwiftRemodel < Formula
   desc "Remodel code generator for Swift"
   homepage "https://github.com/henghonglee/SwiftRemodel"
-  url "https://github.com/henghonglee/SwiftRemodel.git", :tag => "0.1.0", :revision => "0b2f7c79ffe56f1dfa94fda0ebd3257c284c014c"
+  url "https://github.com/henghonglee/SwiftRemodel.git", :tag => "0.1.1", :revision => "867d9e109d884ced20e8f2bd693e45b22759f843"
   head "https://github.com/henghonglee/SwiftRemodel.git"
   depends_on :xcode => ["^10.0", :build]
 
