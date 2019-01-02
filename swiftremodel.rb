ef7cab1c01fbee32773d35b8b5ae8d2bf9af272a
henghonglee <henghong.lee@gmail.com>
2019-01-02 16:26:10 +0800

    Initial commit

---
+++ swiftremodel.rb
@@ -0,0 +1,14 @@
+class SwiftRemodel < Formula
+  desc "Remodel code generator for Swift"
+  homepage "https://github.com/henghonglee/SwiftRemodel"
+  url "https://github.com/henghonglee/SwiftRemodel", :tag => "0.1.0", :revision => "0b2f7c79ffe56f1dfa94fda0ebd3257c284c014c"
+  depends_on :xcode => ["^10.0", :build]
+
+  def install
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
